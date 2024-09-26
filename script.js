0a6080fd2224efaf5dad1fa093f820ad6e52e6bc
unknown <khalidabdi3101@gmail.com>
2024-09-26 05:50:23 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Good Luck 💪🏾
+function getTotalPrice(groceries) {
+    return groceries.reduce((total, item) => total + item.quantity * item.price, 0);
+}
+
+// Example usage:
+console.log(getTotalPrice([{ product: "Milk", quantity: 1, price: 1.50 }])); // ➞ 1.5
+console.log(getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.50 },
+    { product: "Cereals", quantity: 1, price: 2.50 }
+])); // ➞ 4
+
+
+console.log(getTotalPrice([{ product: "Milk", quantity: 3, price: 1.50 }])); // ➞ 4.5
